312777e7460c81082a82554c75520c3d703bcebf
Rodney Osodo <blackd0t@protonmail.com>
2019-05-28 17:07:24 +0300

    Refractoring app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, Response
 import cv2
 
 app = Flask(__name__)
-vc = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
 
 @app.route('/')
 def index():
@@ -14,7 +14,7 @@ def index():
 def gen():
     """Video streaming generator function."""
     while True:
-        rval, frame = vc.read()
+        rval, frame = video.read()
         cv2.imwrite('t.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
@@ -23,8 +23,7 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
