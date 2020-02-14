e96a243a71c9a7b5df3aab1997d91b4bbfcd010f
Mikhail Chichvarin <desertfury@gmail.com>
2020-02-14 14:32:31 +0300

    fix whitespace

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
